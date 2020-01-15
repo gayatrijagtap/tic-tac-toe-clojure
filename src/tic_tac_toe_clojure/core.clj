a858c531306232921eb32d01fae4d373f483976e
gayatrijagtap <gdjagtap1999@gmail.com>
2020-01-15 11:25:40 +0530

    refactor play-game

--- src/tic_tac_toe_clojure/core.clj
+++ src/tic_tac_toe_clojure/core.clj
@@ -1,7 +1,6 @@
 (ns tic-tac-toe-clojure.core)
 (require '[clojure.set])
 
-
 (defn get-player-details [player-num, symbol]
   (println (str "Enter player " player-num " name"))
   {:name (read-line) :symbol symbol :moves #{}}
@@ -25,18 +24,34 @@
        (clojure.string/join "\n--+---+--\n")
        (println)))
 
+(defn get-player-move [player board]
+  (println (str (:name player) " play move."))
+  (let [player-move (read-string (read-line))]
+    (if (invalid-move? player-move board) (do ((println "invalid move.") (get-player-move player board))) player-move)
+    )
+  )
+
+(defn declare-winner [winner board] (do (print-board (vals board)) (println (str (:name winner) " has won!!!"))))
+
+(defn declare-draw [board] (do (print-board (vals board)) (println "Game Draw!!!")))
+
+(defn append-new-move [player new-move] (conj (:moves player) new-move))
+
+(defn update-board [player new-move board] (assoc board new-move (:symbol player)))
+
 (defn play-game [current-player next-player board]
   (print-board (vals board))
-  (println (str (:name current-player) " play move"))
-  (def current-player-move ((comp read-string read-line)))
-  (when (invalid-move? current-player-move board) ((println "invalid move.") (play-game current-player next-player board)))
-  (def updated-player-moves (conj (:moves current-player) current-player-move))
-  (def updated-board (assoc board current-player-move (:symbol current-player)))
-  (cond
-    (has-won? updated-player-moves) (do (print-board (vals updated-board)) (println (str (:name current-player) " has won!!!")) true)
-    (has-draw? board) (do (print-board (vals updated-board)) (println "Game Draw!!!") true)
-    :else (recur next-player (assoc current-player :moves updated-player-moves) updated-board)
-    ))
+  (let [current-player-move (get-player-move current-player board)
+        updated-player-moves (append-new-move current-player current-player-move)
+        updated-board (update-board current-player current-player-move board)
+        ]
+    (cond
+      (has-won? updated-player-moves) (declare-winner current-player updated-board)
+      (has-draw? updated-board) (declare-draw updated-board)
+      :else (recur next-player (assoc current-player :moves updated-player-moves) updated-board)
+      )
+    )
+  )
 
 (defn start-game []
   (play-game (get-player-details 1 "X") (get-player-details 2 "O") initial-board)
